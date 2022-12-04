099bced9b4d0adb153af5690384a1c80db1d3db6
hsuanchi <a0025071@gmail.com>
2022-12-04 12:14:39 +0800

    refactor(index.html): add the cache function for article info part

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+
+def cache_article_info():
+    response = requests.get('https://us-central1-article-maxlist.cloudfunctions.net/get-articles')
+    data = response.json()
+    with open('./static/article.json', 'w') as f:
+        json.dump(data, f, ensure_ascii=False)
+
+if __name__ == '__main__':
+    cache_article_info()
